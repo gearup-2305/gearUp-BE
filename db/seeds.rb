062cd2a1cba7347927f308756cd44d9dc671cc2d
Allen Russell <garyrusselljr@gmail.com>
2023-10-12 13:23:59 -0600

    feat: create working queries

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-painter = Artist.create!(
-  name: "Bob Ross",
-  email: "example@gamil.com",
-  city: "Orlando",
-  state: "FL",
-  zipcode: 32156,
-  password: "password",
-  medium: "Oil",
-  profile_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Bob_Ross%2C_c._1983.jpg/440px-Bob_Ross%2C_c._1983.jpg"
-)
 
-# Posts
-painter.posts.create!(
-  title: "Happy Little Trees",
-  details: "I like to paint happy little trees",
-  image_url: "https://i.pinimg.com/originals/0f/6e/9a/0f6e9a3b2b2b5b0b2b2b2b2b2b2b2b2b2b2b2b2b2b2b2b2.jpg",
-  requested_amount: 100.0,
-  current_amount: 0,
-  artist_id: painter.id
-)
+
+5.times do
+  artist = Artist.create(
+    name: Faker::Artist.name,
+    email: Faker::Internet.email,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    zipcode: Faker::Address.zip_code,
+    password: 'password'
+    medium: Faker::Lorem.word,
+    profile_image: Faker::Avatar.image
+  )
+
+  5.times do
+    artist.posts.create(
+      title: Faker::Lorem.sentence,
+      details: Faker::Lorem.paragraph,
+      image_url: Faker::Internet.url,
+      requested_amount: Faker::Number.decimal(l_digits: 2),
+      current_amount: Faker::Number.decimal(l_digits: 2)
+    )
+  end
+end
