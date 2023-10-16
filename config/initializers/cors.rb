f580a01ac8ad6f9b17031a54611ea1e86fd8a179
Allen Russell <garyrusselljr@gmail.com>
2023-10-16 10:58:05 -0600

    docs: cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://gearup-be-2305-079e2d2dfead.herokuapp.com/' # Replace with your Heroku app's URL
-    resource '*', headers: :any, methods: [:post]
+    origins 'https://gearup-be-2305-079e2d2dfead.herokuapp.com/' 
+    resource '/graphql', headers: :any, methods: [:post, :options]
   end
 end
