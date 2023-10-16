121500c820584a1f40dedf296bd7348ed0acb788
Allen Russell <garyrusselljr@gmail.com>
2023-10-16 11:24:41 -0600

    fix: cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://gearup-be-2305-079e2d2dfead.herokuapp.com/' 
-    resource '/graphql', headers: :any, methods: [:post, :options]
+    origins "*"
+
+    resource "/graphql",
+             headers: :any,
+             methods: %i[get post put patch delete options head]
   end
 end
