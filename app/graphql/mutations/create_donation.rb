e6183fe84344a34ddce29d746e13c3f535a04504
Allen Russell <garyrusselljr@gmail.com>
2023-10-17 12:11:20 -0600

    feat: update_current_amount model method and create_donation mutation
    Co-authored-by: Matthew Lim <matlim200@gmail.com>
    Co-authored-by: Anna Wiley <awiley33@gmail.com>
    Co-authored-by: Andi Lovetto <andilovetto@gmail.com>

---
+++ app/graphql/mutations/create_donation.rb
@@ -0,0 +1,29 @@
+module Mutations
+  class CreateDonation < BaseMutation
+    argument :amount, Float, required: true
+    argument :post_id, Integer, required: true
+    argument :name, String, required: true
+    argument :email, String, required: true
+
+    field :donation, Types::DonationType, null: false
+    field :errors, [String], null: false
+
+    def resolve(amount:, post_id:, name:, email:)
+      post = Post.find(post_id)
+      donation = post.donations.new(amount: amount, post_id: post_id, name: name, email: email)
+      
+      if donation.save
+        post.update_current_amount
+        {
+          donation: donation,
+          errors: []
+        }
+      else
+        {
+          donation: nil,
+          errors: donation.errors.full_messages
+        }
+      end
+    end
+  end
+end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  before do
+    @artist = Artist.create(
+      name: Faker::Artist.name,
+      email: Faker::Internet.email,
+      city: Faker::Address.city,
+      state: Faker::Address.state,
+      zipcode: Faker::Address.zip_code,
+      password: 'password',
+      medium: Faker::TvShows::TheFreshPrinceOfBelAir.quote,
+      profile_image: Faker::Avatar.image
+    )
+
+    @post = @artist.posts.create(
+      title: Faker::TvShows::TheFreshPrinceOfBelAir.quote,
+      details: Faker::TvShows::TheFreshPrinceOfBelAir.quote,
+      image_url: Faker::Internet.url,
+      requested_amount: 1000,
+      current_amount: 0
+    )
+
+  end
+
   describe "relationships" do
     it { should belong_to :artist }
     it { should have_many :donations }
@@ -12,5 +34,22 @@ RSpec.describe Post, type: :model do
     it { should validate_presence_of :image_url }
     it { should validate_presence_of :requested_amount }
     it { should validate_presence_of :current_amount }
+  end
+
+  describe "instance methods" do
+    describe "update_current_amount" do
+      it "updates the current amount of the post" do
+        expect(@post.current_amount).to eq(0)
+
+        donations = @post.donations.create(
+            name: Faker::Hipster.words(number: 1),
+            email: Faker::Internet.email,
+            amount: 200
+          )
+
+        @post.update_current_amount
+        expect(@post.current_amount).to eq(200)
+      end
+    end
   end
 end 
