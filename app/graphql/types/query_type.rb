96272124d0fd054ad8e4480cecc0d9fd4fa3a159
Allen Russell <garyrusselljr@gmail.com>
2023-10-20 12:09:09 -0600

    fix: remove 'limit' from posts query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,7 +4,6 @@ module Types
   class QueryType < Types::BaseObject
     field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
       argument :id, ID, required: true, description: "ID of the object."
-      argument :limit, Int, required: false
     end
 
     def node(id:)
@@ -45,10 +44,6 @@ module Types
         posts = posts.order(created_at: :asc)
       elsif orderBy == "DESC"
         posts = posts.order(created_at: :desc)
-      end
-
-      if limit
-        posts = posts.limit(limit)
       end
 
       posts
