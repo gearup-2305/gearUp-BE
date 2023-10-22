14fc849cd8d1ff23c13cc229c995001266918301
Allen Russell <garyrusselljr@gmail.com>
2023-10-22 17:28:01 -0600

    fix: initializers/sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,10 +7,10 @@
 # end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_TLS_URL'] }  #Heroku Redis URL
+  config.redis = { url: ENV['REDIS_URL'] }  #Heroku Redis URL
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_TLS_URL'] }  #Heroku Redis URL
+  config.redis = { url: ENV['REDIS_URL'] }  #Heroku Redis URL
 end
 
