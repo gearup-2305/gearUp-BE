593edd8c16b4f1a7b31cabdb79acadb4b2d2044a
Allen Russell <garyrusselljr@gmail.com>
2023-10-22 17:05:00 -0600

    fix: using env varialbe which is configured to Heroku using heroku config:set REDIS_URL=<url-from-heroku>

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,16 @@
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: 'redis://localhost:6379/0' }
+# end
+
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: 'redis://localhost:6379/0' }
+# end
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: ENV['REDIS_URL'] }  #Heroku Redis URL
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: ENV['REDIS_URL'] }  #Heroku Redis URL
 end
 
