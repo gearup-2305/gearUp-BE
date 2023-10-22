60a2860594a03b8f81690f51361e5f5336b8d8ad
Allen Russell <garyrusselljr@gmail.com>
2023-10-22 16:51:38 -0600

    fix: remove redis config for sidekiq to use heroku:redis:mini

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,10 +5,4 @@
 # Sidekiq.configure_client do |config|
 #   config.redis = { url: 'redis://localhost:6379/0' }
 # end
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'], network_timeout: 5 }
-end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'], network_timeout: 5 }
-end
